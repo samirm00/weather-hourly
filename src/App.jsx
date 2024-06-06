e60a79bac847136d8c1c7fe52ee53fc5e1293234
samirm00 <sammoham.be@gmail.com>
2024-06-06 12:10:03 +0200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark'
+    }
+});
+
 import Header from './components/Header';
 import WeatherContainer from './components/WeatherContainer';
 
 const App = () => {
     return (
-        <>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
             <Header title="Weather App" />
             <WeatherContainer />
-        </>
+        </ThemeProvider>
     );
 };
 
